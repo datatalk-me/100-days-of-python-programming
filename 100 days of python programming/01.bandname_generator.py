a260b5ffa76e57ad55fabf78f2d4329a390b19b9
Sandeep Rayudu <78495369+datatalk-me@users.noreply.github.com>
2022-10-09 18:31:40 +0530

    Add files via upload

---
+++ 100 days of python programming/01.bandname_generator.py
@@ -0,0 +1,13 @@
+# greeting for the program
+print("Welcome to the Band Name Generator.")
+
+# ask for the city where the user grew up
+city = input('What is the name of the city you grew up in: ')
+
+
+# ask the user to enter the ame of a pet
+pet = input('What is the name of your pet:')
+
+
+#print  the name of the band
+print('your band name couls be' + city + ' ' + pet)
